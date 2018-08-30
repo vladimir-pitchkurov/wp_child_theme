c0dc3406e4d05873af365c7e4ab061f246f67a3f
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-30 23:39:24 +0300

    view for archive-page

--- wp-content/themes/unite-child-theme/archive-movie.php
+++ wp-content/themes/unite-child-theme/archive-movie.php
@@ -1,45 +1,23 @@
-<?php
-/**
- * The template for displaying Archive pages.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package unite
- */
+<?php get_header(); ?>
 
-get_header(); ?>
-
-<section id="primary" class="content-area col-sm-12 col-md-8 <?php echo of_get_option( 'site_layout' ); ?>">
+<section id="primary" class="content-area col-sm-12 <?php echo of_get_option('site_layout'); ?>">
     <main id="main" class="site-main" role="main">
 
-        <?php if ( have_posts() ) : ?>
+        <?php if (have_posts()) : ?>
+        <div class="container">
+            <div class="row">
+                <?php while (have_posts()) : the_post(); ?>
+                    <div class="col-md-4">
+                        <?php get_template_part('content-movie', get_post_format()); ?>
+                    </div>
+                <?php endwhile; ?>
+                <?php unite_paging_nav(); ?>
+                <?php else : ?>
+                    <?php get_template_part('content', 'none'); ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </main>
+</section>
 
-
-            <?php /* Start the Loop */ ?>
-            <?php while ( have_posts() ) : the_post(); ?>
-
-                <h1>IN ARCHIVE_MOVIE</h1>
-                <?php
-                /* Include the Post-Format-specific template for the content.
-                 * If you want to override this in a child theme, then include a file
-                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                 */
-                get_template_part( 'content-movie', get_post_format() );
-                ?>
-                <h1>IN ARCHIVE_MOVIE :: after</h1>
-
-            <?php endwhile; ?>
-
-            <?php unite_paging_nav(); ?>
-
-        <?php else : ?>
-
-            <?php get_template_part( 'content', 'none' ); ?>
-
-        <?php endif; ?>
-
-    </main><!-- #main -->
-</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- wp-content/themes/unite-child-theme/content-movie.php
+++ wp-content/themes/unite-child-theme/content-movie.php
@@ -1,77 +1,83 @@
-<?php
-/**
- * @package unite
- */
-?>
+<!--<h1>I am here? content-movie</h1>-->
 
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('vo_archive-post'); ?>>
     <header class="entry-header page-header">
-
         <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-
-        <?php if ( 'post' == get_post_type() ) : ?>
-            <div class="entry-meta">
-                <?php unite_posted_on(); ?>
-            </div><!-- .entry-meta -->
-        <?php endif; ?>
-    </header><!-- .entry-header -->
-
-    <?php if ( is_search() ) : // Only display Excerpts for Search ?>
+        <?php if (of_get_option('single_post_image', 1) == 1) :
+            the_post_thumbnail('unite-featured', array('class' => 'thumbnail'));
+        endif; ?>
+    </header>
+    <?php if (is_search()) : ?>
         <div class="entry-summary">
             <?php the_excerpt(); ?>
             <p><a class="btn btn-primary read-more"
                   href="<?php the_permalink(); ?>">
-                    <?php _e( 'Continue reading', 'unite' ); ?>
+                    <?php _e('Continue reading', 'unite'); ?>
                     <i class="fa fa-chevron-right"></i></a>
             </p>
-        </div><!-- .entry-summary -->
+        </div>
     <?php else : ?>
         <div class="entry-content">
 
-            <?php if(of_get_option('blog_settings') == 1 || !of_get_option('blog_settings')) : ?>
-                <?php the_content( __( 'Continue reading <i class="fa fa-chevron-right"></i>', 'unite' ) ); ?>
-            <?php elseif (of_get_option('blog_settings') == 2) :?>
+            <?php if (of_get_option('blog_settings') == 1 || !of_get_option('blog_settings')) : ?>
+                <?php the_content(__('Continue reading <i class="fa fa-chevron-right"></i>', 'unite')); ?>
+            <?php elseif (of_get_option('blog_settings') == 2) : ?>
                 <?php the_excerpt(); ?>
             <?php endif; ?>
 
             <?php
-            wp_link_pages( array(
-                'before' => '<div class="page-links">' . __( 'Pages:', 'unite' ),
-                'after'  => '</div>',
-            ) );
+            wp_link_pages(array(
+                'before' => '<div class="page-links">' . __('Pages:', 'unite'),
+                'after' => '</div>',
+            ));
             ?>
-        </div><!-- .entry-content -->
+        </div>
     <?php endif; ?>
 
     <footer class="entry-meta">
-        <?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+        <?php if ('post' == get_post_type()) : // Hide category and tag text for pages on Search ?>
             <?php
-            /* translators: used between list items, there is a space after the comma */
-            $categories_list = get_the_category_list( __( ', ', 'unite' ) );
-            if ( $categories_list && unite_categorized_blog() ) :
+            $categories_list = get_the_category_list(__(', ', 'unite'));
+            if ($categories_list && unite_categorized_blog()) :
                 ?>
                 <span class="cat-links"><i class="fa fa-folder-open-o"></i>
-                    <?php printf( __( ' %1$s', 'unite' ), $categories_list ); ?>
+                    <?php printf(__(' %1$s', 'unite'), $categories_list); ?>
 			</span>
-            <?php endif; // End if categories ?>
+            <?php endif; ?>
 
             <?php
-            /* translators: used between list items, there is a space after the comma */
-            $tags_list = get_the_tag_list( '', __( ', ', 'unite' ) );
-            if ( $tags_list ) :
+            $tags_list = get_the_tag_list('', __(', ', 'unite'));
+            if ($tags_list) :
                 ?>
                 <span class="tags-links"><i class="fa fa-tags"></i>
-                    <?php printf( __( ' %1$s', 'unite' ), $tags_list ); ?>
+                    <?php printf(__(' %1$s', 'unite'), $tags_list); ?>
 			</span>
-            <?php endif; // End if $tags_list ?>
-        <?php endif; // End if 'post' == get_post_type() ?>
+            <?php endif; ?>
+        <?php endif; ?>
+        <div class="row">
+            <?php $post = get_post();
+            $id = get_the_ID();
 
-        <?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-            <span class="comments-link"><i class="fa fa-comment-o"></i><?php comments_popup_link( __( 'Leave a comment', 'unite' ), __( '1 Comment', 'unite' ), __( '% Comments', 'unite' ) ); ?></span>
+            echo '<h5 class="col-md-6">Страна:  ' . get_the_term_list($id, 'country', '', ', ', '') . '</h5>';
+            echo '<h5 class="col-md-6">Жанр:  ' . get_the_term_list($id, 'ganre', '', ', ', '') . '</h5>';
+            echo '<h5 class="col-md-6">Дата выхода: ';
+            foreach (get_post_meta($id, 'date') as $item) {
+                echo $item;
+            };
+            echo '</h5>';
+            echo '<h5 class="col-md-6">Цена: ';
+            foreach (get_post_meta($id, 'price') as $item) {
+                echo $item;
+            };
+            echo '</h5>';
+
+            ?>
+        </div>
+        <?php if (!post_password_required() && (comments_open() || '0' != get_comments_number())) : ?>
+            <span class="comments-link"><i
+                        class="fa fa-comment-o"></i><?php comments_popup_link(__('Leave a comment', 'unite'), __('1 Comment', 'unite'), __('% Comments', 'unite')); ?></span>
         <?php endif; ?>
 
-        <?php edit_post_link( __( 'Edit', 'unite' ), '<i class="fa fa-pencil-square-o"></i><span class="edit-link">', '</span>' ); ?>
-    </footer><!-- .entry-meta -->
-    <hr class="section-divider">
-</article><!-- #post-## -->
+        <?php edit_post_link(__('Edit', 'unite'), '<i class="fa fa-pencil-square-o"></i><span class="edit-link">', '</span>'); ?>
+    </footer>
+</article>
